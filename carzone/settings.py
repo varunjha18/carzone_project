9b068d4ec8e5a6ba7ddccfefd7ecc16ea9ef7f8a
Varun <varunjha2000@gmail.com>
2021-05-21 20:11:26 +0530

    cars app added

--- carzone/settings.py
+++ carzone/settings.py
@@ -30,7 +30,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = ["pages.apps.PagesConfig",
+INSTALLED_APPS = [
+    "pages.apps.PagesConfig",
+    "cars.apps.CarsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
